c144be2b3d5109d3003bd9f7231c41c2740ade82
Maxon1975 <93394865+Maxon1975@users.noreply.github.com>
2022-01-07 18:33:48 +0300

    Create VKPost.java

---
+++ VKPost.java
@@ -0,0 +1,72 @@
+package ru.netology.ru.netology.domain;
+
+
+public class VKPost {
+
+    class LikesInfo {
+    }
+
+    //используем отношение has-a
+    private LikesInfo likesInfo;
+
+    class Account {
+    }
+
+    private Account account;
+
+    class AttachmentPost {
+    }
+
+    private AttachmentPost attachmentPost;
+
+    class CommentsInfo {
+    }
+
+    private CommentsInfo commentsInfo;
+
+    class Copyright {
+    }
+
+    private Copyright copyright;
+
+    class Donut {
+    }
+
+    private Donut donut;
+
+    class GeoPost {
+    }
+
+    private GeoPost geoPost;
+
+    class OwnerPost {
+    }
+
+    private OwnerPost ownerPost;
+
+    class Placeholder {
+    }
+
+    private Placeholder placeholder;
+
+    class Post {
+    }
+
+    private Post post;
+
+    class RepostPost {
+    }
+
+    private RepostPost repostPost;
+
+    class ViewsInfo {
+    }
+
+    private ViewsInfo viewsInfo;
+
+    class WritingOnTheWall {
+    }
+
+    private WritingOnTheWall writingOnTheWall;
+
+}
