41625349880e9c80c9e0db8e50a33f4039660352
Maxon1975 <93394865+Maxon1975@users.noreply.github.com>
2022-01-16 19:00:18 +0300

    Update Post.java

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -1,14 +1,22 @@
 package ru.netology.domain;
 
-public class Post {
-    private int signerId;//Идентификатор автора, если запись была опубликована от имени сообщества и подписана пользователем;
-    private boolean canPin; // Информация о том, может ли текущий пользователь закрепить запись (true — может, false — не может)
-    private boolean canDelete; // Информация о том, может ли текущий пользователь удалить запись (true — может, false — не может);
-    private boolean canEdit; // Информация о том, может ли текущий пользователь редактировать запись (true — может, false — не может);
-    private int isPinned; //Информация о том, что запись закреплена;
-    private boolean markedAsAds;//Информация о том, содержит ли запись отметку «реклама» (true — да, false — нет);
-    private boolean isFavorite;//true, если объект добавлен в закладки у текущего пользователя;
+import ru.netology.domain.CommentsInfo;
+import ru.netology.domain.LikesInfo;
 
-    // boolean true/false
-    //+ getter/setter§
-}
+public class PostInfo {
+    private int postId;
+    private int userId;
+    private int wallId;
+    private int date;
+    private int text;
+    private int photoId;
+    private int soundId;
+    private int repostId;
+    private int commentId;
+    private int likeId;
+    private boolean canPost;
+    private CommentsInfo commentsInfo;
+    private LikesInfo likesInfo;
+    private RepostInfo repostInfo;
+    private ViewsInfo viewsInfo;
+    }
